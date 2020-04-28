111f2bc4a8f8b2b9343ff8235658d2da4975772b
unisys12 <unisys12@gmail.com>
2020-04-28 07:35:37 -0500

    add loose formating method to output html

--- modules/htmlFormt.js
+++ modules/htmlFormt.js
@@ -1,7 +1,26 @@
 module.exports = {
 
     getLore: (data) => {
-        console.log(data);
+        const content = `
+<div class="card">
+    <header>
+        <h1>${data[3]}</h1>
+    </header>
+    <div class="subheader">
+        <h3>Year: ${data[0]}</h3>
+        <h3>Month: ${data[1]}</h3>
+        <h3>Day: ${data[2]}</h3>
+    </div>
+    <article>
+        <p>${data[6]}</p>
+    </article>
+    <footer>
+        <p>${data[5]}</p>
+    </footer>
+</div>
+        `;
+
+        console.log(content)
     }
 
 }
